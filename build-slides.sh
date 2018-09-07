970486d58893f50209b3cb028a52d809784f4f85
b.prioux <b.prioux@lectra.com>
2018-09-07 14:04:54 +0200

    add bdxjug

--- build-slides.sh
+++ build-slides.sh
@@ -13,6 +13,12 @@ docker run --rm -v $(pwd):/documents/ --name asciidoc-to-deckjs binout/docker-as
 echo "Copy resources for devfesttoulouse"
 sed 's/"\/\/cdnjs/"https:\/\/cdnjs/'  output/slides.html > output/devfesttoulouse.html ; cp -R images output
 
+echo "Generate slides for bdxjug"
+docker run --rm -v $(pwd):/documents/ --name asciidoc-to-deckjs binout/docker-asciidoctor-deckjs asciidoctor -a conf=bdxjug -T /asciidoctor-backends/haml/deckjs -D /documents/output slides.adoc
+
+echo "Copy resources for bdxjug"
+sed 's/"\/\/cdnjs/"https:\/\/cdnjs/'  output/slides.html > output/bdxjug.html ; cp -R images output
+
 if [ ! -d "deck.js" ]; then
   echo "Retrieve deck.js"
   mkdir -p output/deck.js ; curl -LkSs https://github.com/imakewebthings/deck.js/archive/1.1.0.tar.gz | tar xfz - -C output/deck.js --strip-components=1
