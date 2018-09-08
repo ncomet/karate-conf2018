895549bec136a73e08aaf02abe6f1d37bdd326f0
Nicolas Comet <nicolas.comet@gmail.com>
2018-09-08 18:26:29 +0200

    basic auth start

---
+++ src/main/kotlin/io/github/binout/karate/config/security/SecurityConfig.kt
@@ -0,0 +1,68 @@
+package io.github.binout.karate.config.security
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint
+import org.springframework.stereotype.Component
+import java.io.PrintWriter
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    @Autowired
+    private val authEntryPoint: MyBasicAuthenticationEntryPoint? = null
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication()
+                .withUser("sensei").password(passwordEncoder().encode("suchWisdom"))
+                .authorities("ROLE_USER")
+    }
+
+    override fun configure(http: HttpSecurity?) {
+        if (http != null) {
+            http.authorizeRequests()
+                    .antMatchers("*/**").anonymous()
+                    .and()
+                    .httpBasic()
+                    .authenticationEntryPoint(authEntryPoint)
+        }
+    }
+
+}
+
+@Component
+class MyBasicAuthenticationEntryPoint : BasicAuthenticationEntryPoint() {
+
+    override fun commence(request: HttpServletRequest, response: HttpServletResponse, authException: AuthenticationException) {
+        response.addHeader("WWW-Authenticate", "Basic realm=$realmName")
+        response.status = HttpServletResponse.SC_UNAUTHORIZED
+        val writer = response.writer
+        writer.println("HTTP Status 401 - " + authException.message)
+    }
+
+    override fun afterPropertiesSet() {
+        realmName = "Karate"
+        super.afterPropertiesSet()
+    }
+
+}
