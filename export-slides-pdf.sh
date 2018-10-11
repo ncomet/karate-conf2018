464039cc86c5c5e99cae316a3648beccf3cad13d
Nicolas Comet <nicolas.comet@gmail.com>
2018-10-11 23:27:39 +0200

    fix pdf generation

--- export-slides-pdf.sh
+++ export-slides-pdf.sh
@@ -2,4 +2,5 @@ mkdir -p output
 
 echo "Generate slides"
 docker run --rm -v $(pwd):/slides amouat/decktape deck https://ncomet.github.io/karate-conf2018/bdxio.html  /slides/output/bdxio.pdf
-docker run --rm -v $(pwd):/slides amouat/decktape deck https://ncomet.github.io/karate-conf2018/devfestoutlouse.html  /slides/output/devfestoutlouse.pdf
+docker run --rm -v $(pwd):/slides amouat/decktape deck https://ncomet.github.io/karate-conf2018/devfesttoulouse.html  /slides/output/devfestoutlouse.pdf
+docker run --rm -v $(pwd):/slides amouat/decktape deck https://ncomet.github.io/karate-conf2018/bdxjug.html  /slides/output/bdxjug.pdf
